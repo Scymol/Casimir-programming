bfbf5ff211d9ab528be85f6aa0df0a12b0318733
Eli Assaf <eli.ams@utexas.edu>
2022-01-31 12:11:40 +0100

    call functions from test.py

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
 print('Hey Peng, happy new year')
+
+import test.py
+
+flt_radius = 3
+perimeter = test.flt_circunference_circle(flt_radius)
+area = test.flt_surface_area_of_circle(flt_radius)
+
+print(perimeter, area)
