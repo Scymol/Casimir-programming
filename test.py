aed9aee626e2cafed4d278c6b544d565c678d0a1
Eli Assaf <eli.ams@utexas.edu>
2022-01-31 12:03:24 +0100

    new function: area of a circle

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 def flt_circunference_circle(flt_radius):
     '''Returns perimeter of a circle'''
     return 6.28318*flt_radius
+
+def flt_surface_area_of_circle(flt_radius):
+    '''Returns area of circle'''
+    return 3.141592*flt_radius*flt_radius
